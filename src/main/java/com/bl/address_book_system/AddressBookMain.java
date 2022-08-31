430246c987fae1cf472e9fb1080dbb06c5b5456a
Charsh177 <harshchougule177@gmail.com>
2022-08-31 11:56:31 +0530

    ADDED: Welcome message

---
+++ src/main/java/com/bl/address_book_system/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bl.address_book_system;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book System");
+    }
+}
